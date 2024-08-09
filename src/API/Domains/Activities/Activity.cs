f85cf62c56857d926558ce1bbac47c953721bbb5
Mamun Reza <Muhammad.Reza@energia.ee>
2024-08-09 13:47:39 +0300

    OMS-10 Activity status added

--- src/API/Domains/Activities/Activity.cs
+++ src/API/Domains/Activities/Activity.cs
@@ -6,10 +6,26 @@ public class Activity
     public string Title { get; set; } = string.Empty;
     public DateTime? ActivityDate { get; set; }
     public string? Description { get; set; }
+    public ActivityStatus Status {  get; set; }
+    public ActivityStatusReason StatusReason { get; set; }
 
     public static class FieldNames
     {
         public const string Id = "activityId";
         public const string Title = "title";
     }
+}
+
+public enum ActivityStatus
+{
+    Open = 1,
+    Completed = 2,
+    Canceled = 3,
+    Scheduled = 4,
+    Deleted = 5
+}
+
+public enum ActivityStatusReason
+{
+    OpenNew = 1, // Status = 1
 }
